281460a71f0d656b625644a81423bfb0cc953ae4
Leon Nikitin <nlv@lab321.ru>
2023-03-31 12:41:38 +0600

    Исправили пересчет остатка плана по продукту

--- manifest.php
+++ manifest.php
@@ -12,9 +12,9 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin (nlv@lab321.com)',
     'description' => 'Добавление ERP функционала в SuiteCRM',
     'is_uninstallable' => true,
-    'published_date' => '2023-03-30',
+    'published_date' => '2023-03-31',
     'type' => 'module',
-    'version' => '0.3.1',
+    'version' => '0.3.2',
     'dependencies' => array(
       )
 );
