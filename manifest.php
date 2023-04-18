255af1aba36eeb1db9a6732cb064137b9989f238
Leon Nikitin <nlv@lab321.ru>
2023-04-18 11:07:38 +0600

    createContract перенесена в функцию

--- manifest.php
+++ manifest.php
@@ -12,9 +12,9 @@ $manifest = array(
     'author' => 'Leon.V.Nikitin (nlv@lab321.com)',
     'description' => 'Добавление ERP функционала в SuiteCRM',
     'is_uninstallable' => true,
-    'published_date' => '2023-04-13',
+    'published_date' => '2023-04-18',
     'type' => 'module',
-    'version' => '0.7.1',
+    'version' => '0.7.2',
     'dependencies' => array(
       )
 );
