ba11563c143971c123600f4762f0bd44e56c0a9d
Leon Nikitin <nlv@lab321.ru>
2023-04-28 16:56:52 +0600

    Техническое поднятие версии до 0.12.2

--- manifest.php
+++ manifest.php
@@ -14,7 +14,7 @@ $manifest = array(
     'is_uninstallable' => true,
     'published_date' => '2023-04-28',
     'type' => 'module',
-    'version' => '0.12.1',
+    'version' => '0.12.2',
     'dependencies' => array(
       )
 );
